1443e67f0b0caeba0eeec4861629cc5fc11365e1
Tom Metcalf <intermet@gmail.com>
2023-06-20 11:54:21 +0100

    complete change email

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,89 +1,88 @@
-import { Text, ScrollView, Button, View, Alert } from "react-native";
-import styles from "../StyleSheet";
-import { useContext, useState } from "react";
-import { useNavigation } from "@react-navigation/native";
-import { deleteUser } from "../utils";
-import { uidContext } from "./Contexts";
+import { Text, ScrollView, Button, View, Alert } from 'react-native';
+import styles from '../StyleSheet';
+import { useContext } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { deleteUser } from '../utils';
+import { uidContext } from './Contexts';
 
-export default function UserProfile({ username, password }) {
-    const email = "Email not found - are you logged in";
-    const currentMap = "not found";
-    const currentMapPercentage = "not connected";
-    const completedMaps = 'not connected';
+export default function UserProfile() {
+  const email = 'Email not found - are you logged in';
+  const currentMap = 'not found';
+  const currentMapPercentage = 'not connected';
+  const completedMaps = 'not connected';
 
-    const navigation = useNavigation();
-    const { user, setUser } = useContext(uidContext);
+  const navigation = useNavigation();
+  const { user, setUser } = useContext(uidContext);
 
-    const handleLogoutPress =() =>{
-        setUser({})
-        navigation.navigate("GeoCachAR")
-        
-    }
-  
-    return (
-        <ScrollView style={styles.userProfileScroll}>
-            <Text style={styles.userProfileEntry}>
-                {user.uid} Username: {user.name}
-            </Text>
-            <View style={styles.userProfileBtnView}>
-                <Button
-                    title="Update"
-                    onPress={() => {
-                        navigation.navigate("Change Username");
-                    }}
-                />
-            </View>
-            <Text style={styles.userProfileEntry}>
-                Email: {user.email ? user.email : email}
-            </Text>
-            <View style={styles.userProfileBtnView}>
-                <Button
-                    title="Update"
-                    onPress={() => Alert.alert("Please contact the developer")}
-                />
-            </View>
-            <Text style={styles.userProfileEntry}>Password: {"********"}</Text>
-            <View style={styles.userProfileBtnView}>
-                <Button
-                    style={styles.homeBtnView}
-                    title="Update"
-                    onPress={() => {
-                        navigation.navigate("Change Password");
-                    }}
-                />
-            </View>
-            <Button
-                title="Delete Account"
-                onPress={() => {
-                    return Alert.alert(
-                        "Delete your account?",
-                        "Are you sure you'd like to delete your account? This action is irreversible",
-                        [
-                            {
-                                text: "Yes",
-                                onPress: () => deleteUser(),
-                            },
-                            { text: "No", style: "cancel" },
-                        ],
-                        { cancelable: true }
-                    );
-                }}
-            />
-            <Text style={styles.userProfileHeader}>Currently Playing:</Text>
-            <Text style={styles.userProfileEntry}>
-                {currentMap} {currentMapPercentage}
-            </Text>
-            <View style={styles.userProfileBtnView}>
-                <Button title="Continue" />
-            </View>
-            <Text style={styles.userProfileHeader}>Completed Maps:</Text>
-            <Text style={styles.userProfileEntry}>{completedMaps}</Text>
-            <View style={styles.userProfileBtnView}>
-                <Button
-                    title="Logout"
-                    onPress={() => handleLogoutPress()}
-                />
-            </View>
-        </ScrollView>
-    );
+  const handleLogoutPress = () => {
+    setUser({});
+    navigation.navigate('GeoCachAR');
+  };
+
+  return (
+    <ScrollView style={styles.userProfileScroll}>
+      <Text style={styles.userProfileEntry}>
+        {/* {user.uid}  */}
+        {'\n'}Username: {user.name}
+      </Text>
+      <View style={styles.userProfileBtnView}>
+        <Button
+          title="Update"
+          onPress={() => {
+            navigation.navigate('Change Username');
+          }}
+        />
+      </View>
+      <Text style={styles.userProfileEntry}>
+        Email: {user.email ? user.email : email}
+      </Text>
+      <View style={styles.userProfileBtnView}>
+        <Button
+          title="Update"
+          onPress={() => navigation.navigate('Change Email')}
+        />
+      </View>
+      <Text style={styles.userProfileEntry}>Password: {'********'}</Text>
+      <View style={styles.userProfileBtnView}>
+        <Button
+          style={styles.homeBtnView}
+          title="Update"
+          onPress={() => {
+            navigation.navigate('Change Password');
+          }}
+        />
+      </View>
+      <View style={styles.userProfileDeleteBtnView}>
+        <Button
+          title="Delete Account"
+          onPress={() => {
+            return Alert.alert(
+              'Delete your account?',
+              "Are you sure you'd like to delete your account? This action is irreversible",
+              [
+                {
+                  text: 'Yes',
+                  onPress: () => deleteUser(),
+                },
+                { text: 'No', style: 'cancel' },
+              ],
+              { cancelable: true }
+            );
+          }}
+        />
+      </View>
+      <Text style={styles.userProfileHeader}>Currently Playing:</Text>
+      <Text style={styles.userProfileEntry}>
+        {currentMap} {currentMapPercentage}
+      </Text>
+      <View style={styles.userProfileBtnView}>
+        <Button title="Continue" />
+      </View>
+      <Text style={styles.userProfileHeader}>Completed Maps:</Text>
+      <Text style={styles.userProfileEntry}>{completedMaps}</Text>
+      <View style={styles.userProfileBtnView}>
+        <Button title="Logout" onPress={() => handleLogoutPress()} />
+      </View>
+    </ScrollView>
+  );
 }
--- components/mapScreen.js
+++ components/mapScreen.js
@@ -1,156 +1,143 @@
 import {
-    Text,
-    Button,
-    TextInput,
-    ScrollView,
-    View,
-    Linking,
-    Alert,
-} from "react-native";
-import styles from "../StyleSheet";
-import MapView, { Marker } from "react-native-maps";
-import { useEffect, useState } from "react";
-import { fetchMap, getUser } from "../utils";
+  Text,
+  Button,
+  TextInput,
+  ScrollView,
+  View,
+  Linking,
+  Alert,
+} from 'react-native';
+import styles from '../StyleSheet';
+import MapView, { Marker } from 'react-native-maps';
+import { useEffect, useState } from 'react';
+import { fetchMap, getUser } from '../utils';
 
 export default function MapScreen({ route }) {
-    const { mapId } = route.params;
-    // mapId = "103";
-    const [congratsMessage, setCongratsMessage] = useState("");
-    const [map, setMap] = useState(false);
-    const [wpNumbers, setWpNumbers] = useState(false);
-    const [numberInputs, setNumberInputs] = useState();
-    const [waypointNumbers, setWaypointNumber] = useState("");
-    const [enteredCode, setEnteredCode] = useState("");
+  const { mapId } = route.params;
+  // mapId = "103";
+  const [congratsMessage, setCongratsMessage] = useState('');
+  const [map, setMap] = useState(false);
+  const [wpNumbers, setWpNumbers] = useState(false);
+  const [numberInputs, setNumberInputs] = useState();
+  const [waypointNumbers, setWaypointNumber] = useState('');
+  const [enteredCode, setEnteredCode] = useState('');
 
-    useEffect(() => {
-        fetchMap(mapId)
-            .then((newMap) => {
-                setMap(newMap);
-                return getUser();
+  useEffect(() => {
+    fetchMap(mapId)
+      .then((newMap) => {
+        setMap(newMap);
+        return getUser();
+      })
+      .then((user) => {
+        setWpNumbers(user.current_maps[mapId]);
+        return user.current_maps[mapId];
+      })
+      .then((wpWaypoints) => {
+        setNumberInputs(
+          Object.keys(wpWaypoints).reduce((numInputs, key) => {
+            return { ...numInputs, [key]: '' };
+          }, {})
+        );
+      })
+      .then(() => {});
+  }, []);
+
+  function handleAlert(wp) {
+    return Alert.alert(`You pressed ${wp}`, map.waypoints[wp].description);
+  }
+
+  return (
+    <>
+      <Text style={styles.availableLocations}>{map.mapName}</Text>
+      {map ? (
+        (() => (
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: map.location.latitude,
+              longitude: map.location.longitude,
+              latitudeDelta: map.location.latDelta,
+              longitudeDelta: map.location.lonDelta,
+            }}
+          >
+            {Object.entries(map.waypoints).map(([index, waypoint]) => {
+              return (
+                <Marker
+                  key={index}
+                  onPress={() => {
+                    handleAlert(index);
+                  }}
+                  title={index}
+                  description=""
+                  coordinate={{
+                    latitude: waypoint.latitude,
+                    longitude: waypoint.longitude,
+                  }}
+                ></Marker>
+              );
+            })}
+          </MapView>
+        ))()
+      ) : (
+        <Text>map loading</Text>
+      )}
+      <View style={styles.launchCamera}>
+        <Button
+          title="Launch Camera"
+          onPress={() => {
+            Linking.openURL(map.arUrl);
+          }}
+        />
+      </View>
+      <ScrollView style={styles.userProfileScroll}>
+        {wpNumbers
+          ? Object.entries(wpNumbers).map(([index, number]) => {
+              return (
+                <View key={index} style={styles.mapScreenDetails}>
+                  <Text style={styles.mapScreenNumber}>{index}</Text>
+                  <TextInput
+                    maxLength={3}
+                    placeholder="submit your code..."
+                    keyboardType="numeric"
+                    style={styles.mapScreenInput}
+                    onChangeText={(value) => setEnteredCode(value)}
+                  />
+                  <View style={styles.mapScreenBtn}>
+                    <Button
+                      title={number ? 'Done' : 'Unlock'}
+                      onPress={() => {
+                        setWpNumbers((curr) => {
+                          console.log(enteredCode, map.waypoints[index]);
+                          if (enteredCode == map.waypoints[index].code) {
+                            return {
+                              ...curr,
+
+                              [index]: true,
+                            };
+                          }
+                          return curr;
+                        });
+                      }}
+                    />
+                  </View>
+                </View>
+              );
             })
-            .then((user) => {
-                setWpNumbers(user.current_maps[mapId]);
-                return user.current_maps[mapId];
-            })
-            .then((wpWaypoints) => {
-                setNumberInputs(
-                    Object.keys(wpWaypoints).reduce((numInputs, key) => {
-                        return { ...numInputs, [key]: "" };
-                    }, {})
-                );
-            })
-            .then(() => {});
-    }, []);
-
-    function handleAlert(wp) {
-        return Alert.alert(`You pressed ${wp}`, map.waypoints[wp].description);
-    }
-
-    return (
-        <>
-            <Text style={styles.availableLocations}>{map.mapName}</Text>
-            {map ? (
-                (() => (
-                    <MapView
-                        style={styles.map}
-                        initialRegion={{
-                            latitude: map.location.latitude,
-                            longitude: map.location.longitude,
-                            latitudeDelta: map.location.latDelta,
-                            longitudeDelta: map.location.lonDelta,
-                        }}
-                    >
-                        {Object.entries(map.waypoints).map(
-                            ([index, waypoint]) => {
-                                return (
-                                    <Marker
-                                        key={index}
-                                        onPress={() => {
-                                            handleAlert(index);
-                                        }}
-                                        title={index}
-                                        description=""
-                                        coordinate={{
-                                            latitude: waypoint.latitude,
-                                            longitude: waypoint.longitude,
-                                        }}
-                                    ></Marker>
-                                );
-                            }
-                        )}
-                    </MapView>
-                ))()
-            ) : (
-                <Text>map loading</Text>
-            )}
-            <View style={styles.launchCamera}>
-                <Button
-                    title="Launch Camera"
-                    onPress={() => {
-                        Linking.openURL(map.arUrl);
-                    }}
-                />
-            </View>
-            <ScrollView style={styles.userProfileScroll}>
-                {wpNumbers
-                    ? Object.entries(wpNumbers).map(([index, number]) => {
-                          return (
-                              <View
-                                  key={index}
-                                  style={styles.mapScreenDetails}
-                              >
-                                  <Text style={styles.mapScreenNumber}>
-                                      {index}
-                                  </Text>
-                                  <TextInput
-                                      maxLength={3}
-                                      placeholder="submit your code..."
-                                      keyboardType="numeric"
-                                      style={styles.mapScreenInput}
-                                      onChangeText={(value) =>
-                                          setEnteredCode(value)
-                                      }
-                                  />
-                                  <View style={styles.mapScreenBtn}>
-                                      <Button
-                                          title={number ? "Done" : "Unlock"}
-                                          onPress={() => {
-                                              setWpNumbers((curr) => {
-                                                  console.log(
-                                                      enteredCode,
-                                                      map.waypoints[index]
-                                                  );
-                                                  if (
-                                                      enteredCode ==
-                                                      map.waypoints[index].code
-                                                  ) {
-                                                      return {
-                                                          ...curr,
-
-                                                          [index]: true,
-                                                      };
-                                                  }
-                                                  return curr;
-                                              });
-                                          }}
-                                      />
-                                  </View>
-                              </View>
-                          );
-                      })
-                    : ""}
-                <Button
-                    title="Submit All Codes!"
-                    onPress={() => {
-                        return Object.values(wpNumbers).every((x) => x)
-                            ? setCongratsMessage(
-                                  "Congratulations! You cracked the code and completed the map!"
-                              )
-                            : setCongratsMessage("");
-                    }}
-                />
-                <Text style={styles.testtest}>{congratsMessage}</Text>
-            </ScrollView>
-        </>
-    );
+          : ''}
+        <View style={styles.submitAllCodesView}>
+          <Button
+            title="Submit All Codes!"
+            onPress={() => {
+              return Object.values(wpNumbers).every((x) => x)
+                ? setCongratsMessage(
+                    'Congratulations! You cracked the code and completed the map!'
+                  )
+                : setCongratsMessage('');
+            }}
+          />
+        </View>
+        <Text style={styles.testtest}>{congratsMessage}</Text>
+      </ScrollView>
+    </>
+  );
 }
