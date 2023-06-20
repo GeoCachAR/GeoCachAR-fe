1c847225f6c6b02f5f3184ef1cc79138d177b73a
steve2116 <s.ackers@hotmail.co.uk>
2023-06-20 15:18:00 +0100

    Fixed sending wrong wpNumbers

--- components/mapScreen.js
+++ components/mapScreen.js
@@ -1,147 +1,163 @@
 import {
-  Text,
-  Button,
-  TextInput,
-  ScrollView,
-  View,
-  Linking,
-  Alert,
-} from 'react-native';
-import styles from '../StyleSheet';
-import MapView, { Marker } from 'react-native-maps';
-import { useContext, useEffect, useState } from 'react';
-import { fetchMap, getUser, patchCode } from '../utils';
-import { uidContext } from './Contexts.js';
-
+    Text,
+    Button,
+    TextInput,
+    ScrollView,
+    View,
+    Linking,
+    Alert,
+} from "react-native";
+import styles from "../StyleSheet";
+import MapView, { Marker } from "react-native-maps";
+import { useContext, useEffect, useState } from "react";
+import { fetchMap, getUser, patchCode } from "../utils";
+import { uidContext } from "./Contexts.js";
 
 export default function MapScreen({ route }) {
-  const { mapId } = route.params;
-  // mapId = "103";
-  const [congratsMessage, setCongratsMessage] = useState('');
-  const [map, setMap] = useState(false);
-  const [wpNumbers, setWpNumbers] = useState(false);
-  const [numberInputs, setNumberInputs] = useState();
-  const [waypointNumbers, setWaypointNumber] = useState('');
-  const [enteredCode, setEnteredCode] = useState('');
-  const { user, setUser } = useContext(uidContext);
+    const { mapId } = route.params;
+    // mapId = "103";
+    const [congratsMessage, setCongratsMessage] = useState("");
+    const [map, setMap] = useState(false);
+    const [wpNumbers, setWpNumbers] = useState(false);
+    const [numberInputs, setNumberInputs] = useState();
+    const [waypointNumbers, setWaypointNumber] = useState("");
+    const [enteredCode, setEnteredCode] = useState("");
+    const { user, setUser } = useContext(uidContext);
 
-  useEffect(() => {
-    fetchMap(mapId)
-      .then((newMap) => {
-        setMap(newMap);
-        return getUser();
-      })
-      .then((user) => {
-        setWpNumbers(user.current_maps[mapId]);
-        return user.current_maps[mapId];
-      })
-      .then((wpWaypoints) => {
-        setNumberInputs(
-          Object.keys(wpWaypoints).reduce((numInputs, key) => {
-            return { ...numInputs, [key]: '' };
-          }, {})
-        );
-      })
-      .then(() => {});
-  }, []);
-
-
-
-  function handleAlert(wp) {
-    return Alert.alert(`You pressed ${wp}`, map.waypoints[wp].description);
-  }
-
-  return (
-    <>
-      <Text style={styles.availableLocations}>{map.mapName}</Text>
-      {map ? (
-        (() => (
-          <MapView
-            style={styles.map}
-            initialRegion={{
-              latitude: map.location.latitude,
-              longitude: map.location.longitude,
-              latitudeDelta: map.location.latDelta,
-              longitudeDelta: map.location.lonDelta,
-            }}
-          >
-            {Object.entries(map.waypoints).map(([index, waypoint]) => {
-              return (
-                <Marker
-                  key={index}
-                  onPress={() => {
-                    handleAlert(index);
-                  }}
-                  title={index}
-                  description=""
-                  coordinate={{
-                    latitude: waypoint.latitude,
-                    longitude: waypoint.longitude,
-                  }}
-                ></Marker>
-              );
-            })}
-          </MapView>
-        ))()
-      ) : (
-        <Text>map loading</Text>
-      )}
-      <View style={styles.launchCamera}>
-        <Button
-          title="Launch Camera"
-          onPress={() => {
-            Linking.openURL(map.arUrl);
-          }}
-        />
-      </View>
-      <ScrollView style={styles.userProfileScroll}>
-        {wpNumbers
-          ? Object.entries(wpNumbers).map(([index, number]) => {
-              return (
-                <View key={index} style={styles.mapScreenDetails}>
-                  <Text style={styles.mapScreenNumber}>{index}</Text>
-                  <TextInput
-                    maxLength={3}
-                    placeholder="submit your code..."
-                    keyboardType="numeric"
-                    style={styles.mapScreenInput}
-                    onChangeText={(value) => setEnteredCode(value)}
-                  />
-                  <View style={styles.mapScreenBtn}>
-                    <Button
-                      title={number ? 'Done' : 'Unlock'}
-                      onPress={() => {
-                        setWpNumbers((curr) => {
-                          if (enteredCode == map.waypoints[index].code) {
-                            return {
-                              ...curr,
-                              [index]: true,
-                            };
-                          }
-                          return curr;
-                        });
-                        patchCode(wpNumbers, mapId, user.uid)
-                      }}
-                    />
-                  </View>
-                </View>
-              );
+    useEffect(() => {
+        fetchMap(mapId)
+            .then((newMap) => {
+                setMap(newMap);
+                return getUser();
             })
-          : ''}
-        <View style={styles.submitAllCodesView}>
-          <Button
-            title="Submit All Codes!"
-            onPress={() => {
-              return Object.values(wpNumbers).every((x) => x)
-                ? setCongratsMessage(
-                    'Congratulations! You cracked the code and completed the map!'
-                  )
-                : setCongratsMessage('');
-            }}
-          />
-        </View>
-        <Text style={styles.testtest}>{congratsMessage}</Text>
-      </ScrollView>
-    </>
-  );
+            .then((user) => {
+                setWpNumbers(user.current_maps[mapId]);
+                return user.current_maps[mapId];
+            })
+            .then((wpWaypoints) => {
+                setNumberInputs(
+                    Object.keys(wpWaypoints).reduce((numInputs, key) => {
+                        return { ...numInputs, [key]: "" };
+                    }, {})
+                );
+            })
+            .then(() => {});
+    }, []);
+
+    function handleAlert(wp) {
+        return Alert.alert(`You pressed ${wp}`, map.waypoints[wp].description);
+    }
+
+    return (
+        <>
+            <Text style={styles.availableLocations}>{map.mapName}</Text>
+            {map ? (
+                (() => (
+                    <MapView
+                        style={styles.map}
+                        initialRegion={{
+                            latitude: map.location.latitude,
+                            longitude: map.location.longitude,
+                            latitudeDelta: map.location.latDelta,
+                            longitudeDelta: map.location.lonDelta,
+                        }}
+                    >
+                        {Object.entries(map.waypoints).map(
+                            ([index, waypoint]) => {
+                                return (
+                                    <Marker
+                                        key={index}
+                                        onPress={() => {
+                                            handleAlert(index);
+                                        }}
+                                        title={index}
+                                        description=""
+                                        coordinate={{
+                                            latitude: waypoint.latitude,
+                                            longitude: waypoint.longitude,
+                                        }}
+                                    ></Marker>
+                                );
+                            }
+                        )}
+                    </MapView>
+                ))()
+            ) : (
+                <Text>map loading</Text>
+            )}
+            <View style={styles.launchCamera}>
+                <Button
+                    title="Launch Camera"
+                    onPress={() => {
+                        Linking.openURL(map.arUrl);
+                    }}
+                />
+            </View>
+            <ScrollView style={styles.userProfileScroll}>
+                {wpNumbers
+                    ? Object.entries(wpNumbers).map(([index, number]) => {
+                          return (
+                              <View
+                                  key={index}
+                                  style={styles.mapScreenDetails}
+                              >
+                                  <Text style={styles.mapScreenNumber}>
+                                      {index}
+                                  </Text>
+                                  <TextInput
+                                      maxLength={3}
+                                      placeholder="submit your code..."
+                                      keyboardType="numeric"
+                                      style={styles.mapScreenInput}
+                                      onChangeText={(value) =>
+                                          setEnteredCode(value)
+                                      }
+                                  />
+                                  <View style={styles.mapScreenBtn}>
+                                      <Button
+                                          title={number ? "Done" : "Unlock"}
+                                          onPress={() => {
+                                              setWpNumbers((curr) => {
+                                                  if (
+                                                      enteredCode ==
+                                                      map.waypoints[index].code
+                                                  ) {
+                                                      patchCode(
+                                                          {
+                                                              ...curr,
+                                                              [index]: true,
+                                                          },
+                                                          mapId,
+                                                          user.uid
+                                                      );
+                                                      return {
+                                                          ...curr,
+                                                          [index]: true,
+                                                      };
+                                                  }
+                                                  return curr;
+                                              });
+                                          }}
+                                      />
+                                  </View>
+                              </View>
+                          );
+                      })
+                    : ""}
+                <View style={styles.submitAllCodesView}>
+                    <Button
+                        title="Submit All Codes!"
+                        onPress={() => {
+                            return Object.values(wpNumbers).every((x) => x)
+                                ? setCongratsMessage(
+                                      "Congratulations! You cracked the code and completed the map!"
+                                  )
+                                : setCongratsMessage("");
+                        }}
+                    />
+                </View>
+                <Text style={styles.testtest}>{congratsMessage}</Text>
+            </ScrollView>
+        </>
+    );
 }
--- utils.js
+++ utils.js
@@ -1,73 +1,75 @@
-import one from './assets/input.js';
-import two from './assets/input2.js';
-import three from './assets/output.js';
-import axios from 'axios';
-const URL = axios.create({ baseURL: 'https://geocachar.onrender.com/api' });
+import one from "./assets/input.js";
+import two from "./assets/input2.js";
+import three from "./assets/output.js";
+import axios from "axios";
+const URL = axios.create({ baseURL: "https://geocachar.onrender.com/api" });
 
 export const changeUsername = (uid, name) => {
-  return URL.patch(`/users/${uid}`, { name }).then(({ data }) => data.name);
+    return URL.patch(`/users/${uid}`, { name }).then(({ data }) => data.name);
 };
 
 export const changeEmail = (newEmail, oldEmail, uid, password) => {
-  console.log(newEmail, oldEmail, password, uid);
-  return URL.patch(`/users/${uid}`, { newEmail, oldEmail, password });
+    console.log(newEmail, oldEmail, password, uid);
+    return URL.patch(`/users/${uid}`, { newEmail, oldEmail, password });
 };
 
 export const changePassword = (uid, newPassword, password, email) => {
-  return new Promise((resolve, reject) => resolve(newPassword));
-  if (!uid) uid = '8Y3o27XpIAfLeOayWqqd3zC4ec83';
-  return URL.patch(`/users/${uid}`, { newPassword, password, email });
+    return new Promise((resolve, reject) => resolve(newPassword));
+    if (!uid) uid = "8Y3o27XpIAfLeOayWqqd3zC4ec83";
+    return URL.patch(`/users/${uid}`, { newPassword, password, email });
 };
 
 export const deleteUser = (uid) => {
-  return;
-  return URL.delete(`/users/${uid}`, { email, password });
+    return;
+    return URL.delete(`/users/${uid}`, { email, password });
 };
 
 export const fetchMap = (mapId) => {
-  return URL.get(`/maps/${mapId}`).then(({ data }) => data.map);
+    return URL.get(`/maps/${mapId}`).then(({ data }) => data.map);
 };
 
 export const fetchMapList = () => {
-  return URL.get('/maps').then(({ data }) => data);
+    return URL.get("/maps").then(({ data }) => data);
 };
 
 export const getUser = (uid) => {
-  return new Promise((resolve, reject) => {
-    resolve({
-      name: 'Default not connected',
-      uid: uid,
-      email: 'thedevelopingdevs@gmail.com',
-      current_maps: {
-        104: {
-          0: false,
-          1: false,
-          2: false,
-          3: false,
-          4: false,
-          5: false,
-          6: false,
-          7: false,
-          8: false,
-          9: false,
-        },
-      },
+    return new Promise((resolve, reject) => {
+        resolve({
+            name: "Default not connected",
+            uid: uid,
+            email: "thedevelopingdevs@gmail.com",
+            current_maps: {
+                104: {
+                    0: false,
+                    1: false,
+                    2: false,
+                    3: false,
+                    4: false,
+                    5: false,
+                    6: false,
+                    7: false,
+                    8: false,
+                    9: false,
+                },
+            },
+        });
     });
-  });
-  return URL.get(`/users/${uid}`).then(({ data }) => data.user);
+    return URL.get(`/users/${uid}`).then(({ data }) => data.user);
 };
 
 export const authenticate = (email, password) => {
-  return URL.post('/account', { email, password }).then(({ data }) => data.uid);
+    return URL.post("/account", { email, password }).then(
+        ({ data }) => data.uid
+    );
 };
 
 export const signup = (username, email, password) => {
-  return URL.post('/users', { name: username, email, password }).then(
-    ({ data }) => data.uid
-  );
+    return URL.post("/users", { name: username, email, password }).then(
+        ({ data }) => data.uid
+    );
 };
 
 export const patchCode = (wpNumbers, mapId, uid) => {
-  console.log({[mapId]: wpNumbers}, uid,  '<<<<<<<<<<-------------')
-  return URL.patch(`/users/${uid}/current_maps/${mapId}`, {[mapId]: wpNumbers})
-}
+    console.log({ [mapId]: wpNumbers }, uid, "<<<<<<<<<<-------------");
+    return URL.patch(`/users/${uid}`, { current_map: { [mapId]: wpNumbers } });
+};
